6ee90a3f1355cea8c05efc2d363b1285f4f90477
Dmand723 <dawson.simmons723@gmail.com>
2024-12-05 12:34:31 -0700

    Finished TODO site

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,25 +1,31 @@
 //Video Timestamp 46:28
+
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
-
+const connectDB = require("./db/connect");
+require("dotenv").config();
+const notFound = require("./middleware/not-found");
+const errorhandlerMiddleware = require("./middleware/error-handler");
 // middleware
-
+app.use(express.static("./public"));
 app.use(express.json());
 
-//routs
-app.get("/hello", (req, res) => {
-  res.send("Tast Manager app");
-});
-
+//routes
 app.use("/api/v1/tasks", tasks);
 
-//app.get('/api/v1/tasks') -get all the tasks
-//app.post('/api/v1/tasks') -create a new task
-//app.get('/api/v1/tasks/:id') - get single task
-//app.patch('/api/v1/tasks/:id') - update task
-//app.delete('/api/v1/tasks/:id')- delete task
+app.use(notFound);
+app.use(errorhandlerMiddleware);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(port, console.log(`server is listening on port ${port}...`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
